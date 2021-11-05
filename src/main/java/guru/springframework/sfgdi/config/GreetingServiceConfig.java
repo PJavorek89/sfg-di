03b974b4b6788b6b3d1d141c88095bc85f57fa51
Pavel Javorek <javorek2@komix.cz>
2021-11-05 14:01:39 +0100

    end of l. 74

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,46 @@
+package guru.springframework.sfgdi.config;
+
+import guru.springframework.sfgdi.services.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18NSpanishService i18NSpanishService(){
+        return new I18NSpanishService();
+    }
+
+
+
+    @Primary //ucelem tohoto je zjistit jestli je tento stereotyp schopen zvladat i profily
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
+    @Bean //the returned object will be marked as a bean, used when i work with 3. party app
+    ConstructorGreetingService constructorGreetingService(){
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService(){
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService(){
+        return new SetterInjectedGreetingService();
+    }
+}
